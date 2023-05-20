9cf5d5a3fd602fd78bbf0bf03873773fa5ac9b71
Kusalava0 <dkb03122001@gmail.com>
2023-05-20 17:05:26 +0530

    environment variables changed

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,20 +1,20 @@
 from typing import List
-
+import os
 from decouple import config
 from pydantic import AnyHttpUrl, BaseSettings
 
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    JWT_SECRET_KEY: str = config("JWT_SECRET_KEY", cast=str)
-    JWT_REFRESH_SECRET_KEY: str = config("JWT_REFRESH_SECRET_KEY", cast=str)
+    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
+    JWT_REFRESH_SECRET_KEY: str = os.getenv("JWT_REFRESH_SECRET_KEY")
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7   # 7 days
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
         "http://localhost:3000"
     ]
-    PROJECT_NAME: str = "FODOIST"
+    PROJECT_NAME: str = "TODO LIST"
     
     # Database
     MONGO_CONNECTION_STRING: str = config("MONGO_CONNECTION_STRING", cast=str)
